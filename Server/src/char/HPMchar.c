cb2cb620c04f84aaa16197128f3e06c304885775
Finger Nailbiter <iam.nailbiter@gmail.com>
2015-11-17 15:32:12 +0700

    Base Server
    Base Hercules Server

---
+++ Server/src/char/HPMchar.c
@@ -0,0 +1,78 @@
+// Copyright (c) Hercules Dev Team, licensed under GNU GPL.
+// See the LICENSE file
+
+#define HERCULES_CORE
+
+#include "HPMchar.h"
+
+#include "common/HPM.h"
+#include "common/cbasetypes.h"
+
+#include "char/char.h"
+#include "char/geoip.h"
+#include "char/inter.h"
+#include "char/int_auction.h"
+#include "char/int_elemental.h"
+#include "char/int_guild.h"
+#include "char/int_homun.h"
+#include "char/int_mail.h"
+#include "char/int_mercenary.h"
+#include "char/int_party.h"
+#include "char/int_pet.h"
+#include "char/int_quest.h"
+#include "char/int_storage.h"
+#include "char/loginif.h"
+#include "char/mapif.h"
+#include "char/pincode.h"
+#include "common/HPMi.h"
+#include "common/conf.h"
+#include "common/console.h"
+#include "common/core.h"
+#include "common/db.h"
+#include "common/des.h"
+#include "common/ers.h"
+#include "common/memmgr.h"
+#include "common/mapindex.h"
+#include "common/mmo.h"
+#include "common/nullpo.h"
+#include "common/showmsg.h"
+#include "common/socket.h"
+#include "common/spinlock.h"
+#include "common/sql.h"
+#include "common/strlib.h"
+#include "common/sysinfo.h"
+#include "common/timer.h"
+#include "common/utils.h"
+
+// HPMDataCheck comes after all the other includes
+#include "common/HPMDataCheck.h"
+
+/**
+ * HPM plugin data store validator sub-handler (char-server)
+ *
+ * @see HPM_interface::data_store_validate
+ */
+bool HPM_char_data_store_validate(enum HPluginDataTypes type, struct hplugin_data_store **storeptr, bool initialize)
+{
+	switch (type) {
+		// No supported types at the moment.
+		default:
+			break;
+	}
+	return false;
+}
+
+void HPM_char_plugin_load_sub(struct hplugin *plugin) {
+	plugin->hpi->sql_handle = inter->sql_handle;
+}
+
+void HPM_char_do_init(void) {
+	HPM->load_sub = HPM_char_plugin_load_sub;
+	HPM->data_store_validate_sub = HPM_char_data_store_validate;
+	HPM->datacheck_init(HPMDataCheck, HPMDataCheckLen, HPMDataCheckVer);
+	HPM_shared_symbols(SERVER_TYPE_CHAR);
+}
+
+void HPM_char_do_final(void) {
+	HPM->datacheck_final();
+}
---
+++ Server/src/login/HPMlogin.c
@@ -0,0 +1,63 @@
+// Copyright (c) Hercules Dev Team, licensed under GNU GPL.
+// See the LICENSE file
+
+#define HERCULES_CORE
+
+#include "HPMlogin.h"
+
+#include "common/HPM.h"
+#include "common/cbasetypes.h"
+
+#include "login/account.h"
+#include "login/login.h"
+#include "common/HPMi.h"
+#include "common/conf.h"
+#include "common/console.h"
+#include "common/core.h"
+#include "common/db.h"
+#include "common/des.h"
+#include "common/ers.h"
+#include "common/memmgr.h"
+#include "common/mmo.h"
+#include "common/nullpo.h"
+#include "common/showmsg.h"
+#include "common/socket.h"
+#include "common/spinlock.h"
+#include "common/sql.h"
+#include "common/strlib.h"
+#include "common/sysinfo.h"
+#include "common/timer.h"
+#include "common/utils.h"
+
+// HPMDataCheck comes after all the other includes
+#include "common/HPMDataCheck.h"
+
+/**
+ * HPM plugin data store validator sub-handler (login-server)
+ *
+ * @see HPM_interface::data_store_validate
+ */
+bool HPM_login_data_store_validate(enum HPluginDataTypes type, struct hplugin_data_store **storeptr, bool initialize)
+{
+	switch (type) {
+		// No supported types at the moment
+		default:
+			break;
+	}
+	return false;
+}
+
+void HPM_login_plugin_load_sub(struct hplugin *plugin) {
+	plugin->hpi->sql_handle = account_db_sql_up(login->accounts);
+}
+
+void HPM_login_do_init(void) {
+	HPM->load_sub = HPM_login_plugin_load_sub;
+	HPM->data_store_validate_sub = HPM_login_data_store_validate;
+	HPM->datacheck_init(HPMDataCheck, HPMDataCheckLen, HPMDataCheckVer);
+	HPM_shared_symbols(SERVER_TYPE_LOGIN);
+}
+
+void HPM_login_do_final(void) {
+	HPM->datacheck_final();
+}
